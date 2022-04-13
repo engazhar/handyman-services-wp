4c0bc379f5eb338eba6305eb1a9c615d5a75e25f
Azhar <eng.azharkhan@gmail.com>
2022-04-13 07:08:54 +0500

    Basic Plugin header Requirement

---
+++ handyman-services.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       HandyMan Services
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Book House repair and maintenance services.
+ * Version:           1.0.0
+ * Requires PHP:      7.2
+ * Author:            Azhar Nazir
+ * Author URI:        https://magenta-dodol-530e69.netlify.app/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       handyman-services
+ * 
+ */
